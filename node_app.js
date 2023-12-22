c50338cfb1494dc78d7db434dfdd2a9a70b62bb4
arulmanoharan <56474371+arulmanoharan@users.noreply.github.com>
2023-12-22 20:41:09 +0530

    Update node_app.js

--- node_app.js
+++ node_app.js
@@ -1,189 +1,47 @@
-const express = require('express');
-const mysql = require('mysql');
-const bodyParser = require('body-parser');
+onst express = require('express');
 const { SecretManagerServiceClient } = require('@google-cloud/secret-manager');
 
 const app = express();
-const port = process.env.PORT;
-
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-
+const port = process.env.PORT || 8080;
 
 async function getSecret() {
   const client = new SecretManagerServiceClient();
   const projectId = process.env.GCP_PROJECT_ID; 
-  const secretName = process.env.SECRET_NAME; 
-
-  
-try{
-  const [version] = await client.accessSecretVersion({
-    name: `projects/${projectId}/secrets/${secretName}/versions/latest`,
-  });
-      return version.payload.data.toString('utf8');
-  } catch (err) {
-    console.error('Error fetching secret:', err);
-    return null;
-  }
-}
-
-async function createConnection() {
-  const secretValue = await getSecret();
-
-  if (!secretValue) {
-    console.error('Secret value is null or undefined. Unable to establish database connection.');
-    return null;
-  }
+  const secretName = process.env.SECRET_NAME;
 
   try {
-    const config = JSON.parse(secretValue);
+    const [version] = await client.accessSecretVersion({
+    name: `projects/${projectId}/secrets/${secretName}/versions/latest`,
+  });
 
-    const connection = await mysql.createConnection({
-      host: config.host,
-      user: config.user,
-      password: config.password,
-      database: config.database,
-    });
-
-    return connection;
-  } catch (error) {
-    console.error('Error creating database connection:', error);
-    return null;
+    return version.payload.data.toString('utf8');
+  } catch (err) {
+    console.error('Error fetching secret:', err);
+    return 'Error fetching secret';
   }
 }
 
 app.get('/', async (req, res) => {
-  const connection = await createConnection();
-  connection.query(
-  `CREATE TABLE IF NOT EXISTS uname (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255)
-  )`,
-  (error) => {
-    if (error) throw error;
-    console.log('Table created or already exists');
-  }
-);
+  const secretValue = await getSecret();
+  const secretObject = JSON.parse(secretValue);
+  // Send an HTML page with the secret value
+  const htmlPage = `
+    <!DOCTYPE html>
+    <html>
+      <head>
+        <title>Secret Display Page</title>
+      </head>
+      <body>
+        <h1>Secret Value:</h1>
+        <p>${secretObject.connection_name}</p>
+        <p>${secretObject.username}</p>
+        <p>${secretObject.password}</p>
+        <p>${secretObject.database}</p>
+      </body>
+    </html>
+  `;
 
-  if (connection) {
-    try {
-      // Fetch the list of names from the 'uname' table
-      const [rows] = await connection.execute('SELECT * FROM uname');
-
-      // Render the HTML page with the list of names and a form to add new names
-      const htmlPage = `
-        <!DOCTYPE html>
-        <html>
-          <head>
-            <title>Name List</title>
-          </head>
-          <body>
-            <h1>Name List:</h1>
-            <table border="1">
-              <tr>
-                <th>ID</th>
-                <th>Name</th>
-              </tr>
-              ${rows.map(row => `
-                <tr>
-                  <td>${row.id}</td>
-                  <td>${row.name}</td>
-                </tr>`).join('')}
-            </table>
-            <h2>Add New Name:</h2>
-            <form action="/addname" method="post">
-              <label for="newName">Name:</label>
-              <input type="text" id="newName" name="newName" required>
-              <button type="submit">Add Name</button>
-            </form>
-            <h2>Print Names:</h2>
-            <form action="/printnames" method="get">
-              <button type="submit">Print Names</button>
-            </form>
-          </body>
-        </html>
-      `;
-
-      res.send(htmlPage);
-    } catch (error) {
-      console.error('Error fetching name list:', error);
-      res.status(500).send('Internal Server Error');
-    } finally {
-      // Close the database connection after use
-      connection.end();
-    }
-  } else {
-    // Connection failed, send an error response
-    res.status(500).send('Internal Server Error');
-  }
-});
-
-app.post('/addname', async (req, res) => {
-  const connection = await createConnection();
-
-  if (connection) {
-    try {
-      const { newName } = req.body;
-
-      if (!newName) {
-        res.status(400).send('Name is required.');
-        return;
-      }
-
-      // Insert the new name into the 'uname' table
-      await connection.execute('INSERT INTO uname (name) VALUES (?)', [newName]);
-
-      // Redirect back to the root route to display the updated name list
-      res.redirect('/');
-    } catch (error) {
-      console.error('Error adding new name:', error);
-      res.status(500).send('Internal Server Error');
-    } finally {
-      // Close the database connection after use
-      connection.end();
-    }
-  } else {
-    // Connection failed, send an error response
-    res.status(500).send('Internal Server Error');
-  }
-});
-
-app.get('/printnames', async (req, res) => {
-  const connection = await createConnection();
-
-  if (connection) {
-    try {
-      // Fetch the list of names from the 'uname' table
-      const [rows] = await connection.execute('SELECT * FROM uname');
-
-      // Render the HTML page with the list of names
-      const htmlPage = `
-        <!DOCTYPE html>
-        <html>
-          <head>
-            <title>Print Names</title>
-          </head>
-          <body>
-            <h1>Names:</h1>
-            <ul>
-              ${rows.map(row => `<li>${row.name}</li>`).join('')}
-            </ul>
-          </body>
-        </html>
-      `;
-
-      res.send(htmlPage);
-    } catch (error) {
-      console.error('Error fetching name list:', error);
-      res.status(500).send('Internal Server Error');
-    } finally {
-      // Close the database connection after use
-      connection.end();
-    }
-  } else {
-    // Connection failed, send an error response
-    res.status(500).send('Internal Server Error');
-  }
+  res.send(htmlPage);
 });
 
 app.listen(port, () => {
