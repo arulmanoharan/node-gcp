a81a42d5e2f74507d0f97af4b7da3fe9ced16ea9
arulmanoharan <56474371+arulmanoharan@users.noreply.github.com>
2023-12-22 19:49:10 +0530

    Update node_app.js

--- node_app.js
+++ node_app.js
@@ -1,124 +1,44 @@
 const express = require('express');
-const mysql = require('mysql');
-const bodyParser = require('body-parser');
 const { SecretManagerServiceClient } = require('@google-cloud/secret-manager');
 
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-
-
-async function accessSecret() {
-  const projectId = process.env.GCP_PROJECT_ID; 
-  const secretName = process.env.SECRET_NAME; 
-
+async function getSecret() {
   const client = new SecretManagerServiceClient();
-  const [version] = await client.accessSecretVersion({
-    name: `projects/${projectId}/secrets/${secretName}/versions/latest`,
-  });
 
-  const secretValue = version.payload.data.toString('utf8');
-  const secretObject = JSON.parse(secretValue);
+  try {
+    const [version] = await client.accessSecretVersion({
+      name: 'projects/absolute-range-408808/secrets/cloudsql-secrets/versions/latest'
+    });
 
-  const connection = mysql.createConnection({
-    host: secretObject.connection_name,
-    user: secretObject.username,
-    password: secretObject.password,
-    database: secretObject.database_name,
-  });
-
-// Create a table if it doesn't exist
-connection.query(
-  `CREATE TABLE IF NOT EXISTS uname (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255)
-  )`,
-  (error) => {
-    if (error) throw error;
-    console.log('Table created or already exists');
+    return version.payload.data.toString('utf8');
+  } catch (err) {
+    console.error('Error fetching secret:', err);
+    return 'Error fetching secret';
   }
-);
+}
 
-app.get('/', (req, res) => {
-  res.send(`
+app.get('/', async (req, res) => {
+  const secretValue = await getSecret();
+
+  // Send an HTML page with the secret value
+  const htmlPage = `
+    <!DOCTYPE html>
     <html>
+      <head>
+        <title>Secret Display Page</title>
+      </head>
       <body>
-        <form action="/api/add" method="post">
-          <label for="name">Enter your name:</label>
-          <input type="text" id="name" name="name">
-          <button type="submit">Submit Name</button>
-        </form>
-
-        <button onclick="fetchData()">Fetch Data</button>
-        <div id="data-container"></div>
-
-        <script>
-          async function fetchData() {
-            try {
-              const response = await fetch('/api/data');
-              const data = await response.json();
-
-              // Display the data in the 'data-container' div
-              const dataContainer = document.getElementById('data-container');
-              dataContainer.innerHTML = '<h2>Data from Cloud SQL Database:</h2>';
-
-              if (data.length === 0) {
-                dataContainer.innerHTML += '<p>No data available.</p>';
-              } else {
-                dataContainer.innerHTML += '<ul>';
-                data.forEach(item => {
-                  dataContainer.innerHTML += '<li>' + item.name + '</li>';
-                });
-                dataContainer.innerHTML += '</ul>';
-              }
-            } catch (error) {
-              console.error('Error fetching data:', error);
-            }
-          }
-        </script>
+        <h1>Secret Value:</h1>
+        <p>${secretValue}</p>
       </body>
     </html>
-  `);
-});
+  `;
 
-app.post('/api/add', (req, res) => {
-  const { name } = req.body;
-
-  if (!name) {
-    res.status(400).json({ error: 'Name is required' });
-    return;
-  }
-
-  // Insert the name into the database
-  connection.query('INSERT INTO uname (name) VALUES (?)', [name], (error) => {
-    if (error) {
-      console.error(error);
-      res.status(500).json({ error: 'Internal Server Error' });
-      return;
-    }
-
-    res.redirect('/');
-  });
-});
-
-app.get('/api/data', (req, res) => {
-  // Fetch data from the database
-  connection.query('SELECT name FROM uname', (error, results) => {
-    if (error) {
-      console.error(error);
-      res.status(500).json({ error: 'Internal Server Error' });
-      return;
-    }
-
-    // Send the data as JSON
-    res.json(results);
-  });
+  res.send(htmlPage);
 });
 
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Server is running on port ${port}`);
 });
-}
-accessSecret();
