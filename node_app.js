55b8a7f4a9dc79128f5122614257eb6d90a8ba76
arulmanoharan <56474371+arulmanoharan@users.noreply.github.com>
2023-12-22 17:33:25 +0530

    Update node_app.js

--- node_app.js
+++ node_app.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const { SecretManagerServiceClient } = require('@google-cloud/secret-manager');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
